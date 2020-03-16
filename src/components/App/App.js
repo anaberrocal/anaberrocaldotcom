3c41ece502d3317b844822fc14a806caad73022e
Ana Berrocal <anaberrocalmail@gmail.com>
2020-03-15 20:35:36 -0400

    Organize animations into 'HomePage' files and import into 'App'

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import HomePage from "../HomePage/HomePage.js";
+import './style.css';
+ 
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+      
+        <HomePage />  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './HomePage.css';
+import Bubble1 from '../../assests/Bubble-1.png';
+import Bubble2 from '../../assests/Bubble-2.png';
+import Bubble3 from '../../assests/Bubble-3.png';
+import Bubble4 from '../../assests/Bubble-4.png';
+import Grid1 from '../../assests/Grid-1.png';
+import Grid2 from '../../assests/Grid-2.png';
+import Plus1 from '../../assests/Plus-Sign-1.png';
+import Plus2 from '../../assests/Plus-Sign-2.png';
+import Plus3 from '../../assests/Plus-Sign-3.png';
+import Line1 from '../../assests/Line-1.png';
+import Line2 from '../../assests/Line-1.png';
+import Title from '../../assests/Title-Intro.png';
+import styled, { keyframes } from 'styled-components';
+import { fadeInRight } from 'react-animations';
+
+
+
+const Fade = styled.div`animation: 3s ${keyframes`${fadeInRight}`} `;
+
+
+
+export default function HomePage() {
+  return (
+    <div className="HomePage-Animations-Container">
+        <div className="top-left">  
+            <Fade><img src={Bubble1} className="bubble-1"/></Fade>
+            <Fade><img src={Grid1} className="grid-1"/></Fade>
+            
+        </div>
+
+        <div className="top-right"> 
+            <Fade><img src={Bubble2} className="bubble-2"/></Fade>
+            <Fade><img src={Plus1} className="plus-1"/></Fade>
+            <Fade><img src={Plus2} className="plus-2"/></Fade>
+        </div>
+
+        <div className="titleContainer">
+            <Fade><img src={Title} className="title"/></Fade>
+            <Fade><img src={Line1} className="line-1"/></Fade>
+            <Fade><img src={Line2} className="line-2"/></Fade>  
+        </div>
+
+        <div className="bottom-right"> 
+            <Fade><img src={Bubble3} className="bubble-3"/></Fade>
+            <Fade><img src={Grid2} className="grid-2"/></Fade>
+        </div>
+
+        <div className="bottom-left"> 
+        <Fade><img src={Bubble4} className="bubble-4"/></Fade>
+        <Fade><img src={Plus3} className="plus-3"/></Fade>
+        </div>
+    </div>
+  );
+}
