97e20b351142fc611fda80fa6323e221acc75644
Ana Berrocal <anaberrocalmail@gmail.com>
2020-03-18 13:35:09 -0400

    Add, style and animate scroll arrow to home page and install material-ui/icons

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './newHomePage.css';
+import './HomePage.css';
 import Bubble1 from '../../assests/Bubble-1.png';
 import Bubble2 from '../../assests/Bubble-2.png';
 import Bubble3 from '../../assests/Bubble-3.png';
@@ -12,18 +12,30 @@ import Plus3 from '../../assests/Plus-Sign-3.png';
 import Line1 from '../../assests/Line-1.png';
 import Line2 from '../../assests/Line-1.png';
 import Title from '../../assests/Title-Intro.png';
+// import Ana from '../../assests/Ana.png';
 import styled, { keyframes } from 'styled-components';
 import { fadeInRight } from 'react-animations';
+// import LinkedInIcon from '@material-ui/icons/LinkedIn';
+// import GitHubIcon from '@material-ui/icons/GitHub';
+// import YouTubeIcon from '@material-ui/icons/YouTube';
+// import InstagramIcon from '@material-ui/icons/Instagram';
+
 
 
 
 const Fade = styled.div`animation: 3.5s ${keyframes`${fadeInRight}`} `;
 
-
-
 export default function HomePage() {
   return (
     <div id="HomePage-Animations-Container">
+        {/* <img src={Ana} className="headshot"/>
+        <span className="Social-Icons">
+            <LinkedInIcon className="linkedIn"/>
+            <GitHubIcon className="GitHub"/>
+            <YouTubeIcon className="YouTube"/>
+            <InstagramIcon className="Instagram"/>
+        </span> */}
+        
         <span className="top-left">  
             <img src={Bubble1} className="bubble-1"/>
             <img src={Grid1} className="grid-1"/>
@@ -51,6 +63,9 @@ export default function HomePage() {
         <img src={Bubble4} className="bubble-4"/>
         <img src={Plus3} className="plus-3"/>
         </span>
+        <section id="scrollButton" class="scroll">
+        <a href="#section06"><span></span></a>
+        </section>
         
     </div>
   );
