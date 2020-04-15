79284a4e850679cffec6c2582b06725c0011407c
Ana Berrocal <anaberrocalmail@gmail.com>
2020-04-14 22:30:28 -0400

    Updates

---
+++ precache-manifest.fd6a8dd68aace80c68f8e73498a09815.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd2d08bd02cdad0765b08415273592b",
+    "url": "/anaberrocaldotcom/index.html"
+  },
+  {
+    "revision": "994a6556b3c7a3cb3710",
+    "url": "/anaberrocaldotcom/static/css/main.c8e14be4.chunk.css"
+  },
+  {
+    "revision": "acd7a899f75f71240c36",
+    "url": "/anaberrocaldotcom/static/js/2.24af1372.chunk.js"
+  },
+  {
+    "revision": "994a6556b3c7a3cb3710",
+    "url": "/anaberrocaldotcom/static/js/main.25509421.chunk.js"
+  },
+  {
+    "revision": "89f649b8b5f4c1dd203b",
+    "url": "/anaberrocaldotcom/static/js/runtime-main.b0234826.js"
+  },
+  {
+    "revision": "1580e3bc257bfdaa7e9f7afb9940f4cd",
+    "url": "/anaberrocaldotcom/static/media/Bubble-1.1580e3bc.png"
+  },
+  {
+    "revision": "9481dd850790b794f2250c2c7232c902",
+    "url": "/anaberrocaldotcom/static/media/Bubble-2.9481dd85.png"
+  },
+  {
+    "revision": "7f31ff9dc598bdd193926fdac183acfd",
+    "url": "/anaberrocaldotcom/static/media/Bubble-3.7f31ff9d.png"
+  },
+  {
+    "revision": "a72d114bdc2467f683905f02f5eb3afb",
+    "url": "/anaberrocaldotcom/static/media/Bubble-4.a72d114b.png"
+  },
+  {
+    "revision": "6d77e005b9fc97b337253e35686d704a",
+    "url": "/anaberrocaldotcom/static/media/Distanceware.6d77e005.gif"
+  },
+  {
+    "revision": "fc2f537886435ec67457cce3efffa1b0",
+    "url": "/anaberrocaldotcom/static/media/FindTheFood.fc2f5378.gif"
+  },
+  {
+    "revision": "e76c421202c11f943c6d87738f7faff5",
+    "url": "/anaberrocaldotcom/static/media/Grid-1.e76c4212.png"
+  },
+  {
+    "revision": "6fdb8e506f611fa1eeb140c64d5f5742",
+    "url": "/anaberrocaldotcom/static/media/Grid-2.6fdb8e50.png"
+  },
+  {
+    "revision": "7c8fa71a6aa3a9e2b71a6458ce6e0d67",
+    "url": "/anaberrocaldotcom/static/media/Portfolio-Favicon.7c8fa71a.png"
+  },
+  {
+    "revision": "8072e7d1789c3f2709257064fc513a97",
+    "url": "/anaberrocaldotcom/static/media/Title-Intro.8072e7d1.png"
+  }
+]);
