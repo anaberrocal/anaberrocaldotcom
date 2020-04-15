0818f8092edf16d9997da14727d92c0cf54cdedd
Ana Berrocal <anaberrocalmail@gmail.com>
2020-04-14 22:02:00 -0400

    run build

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -8,21 +8,30 @@ export default function Projects() {
   return (
     <div className="projects">
         <div id="projectsTitle">
-          <h1 className="myProjects">These are some of the projects I have worked on</h1>
         </div>
       <div className="projectsContainer">
 
         <div className="project">
-          <h1 className="projectName">Find The Food</h1>
-          <img id="projectImage" src={findTheFood}/>
-          <div className="projectLink"><a href="https://anaberrocal.github.io/FindTheFood/" target="_blank">Demo</a></div>
-            <div className="projectLink"><a href="https://github.com/anaberrocal/FindTheFood" target="_blank">GitHub</a></div>
-
-        <div className="project">
+        <h1 className="myProjects">Work Experience</h1>
         <h1 className="projectName">Distanceware</h1>
             <img id="projectImage" src={Distanceware} />
             <div className="projectLink"><a href="https://distanceware.com/" target="_blank">Distanceware.com</a></div>
         </div>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+        
+        <div className="project">
+        <h1 className="myProjects">Personal Projects</h1>
+          <h1 className="projectName">Find The Food</h1>
+          <img id="projectImage" src={findTheFood}/>
+          <div className="projectLink"><a href="https://anaberrocal.github.io/FindTheFood/" target="_blank">Demo</a></div>
+            <div className="projectLink"><a href="https://github.com/anaberrocal/FindTheFood" target="_blank">GitHub</a></div>
 
         </div>
       </div>
